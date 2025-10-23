8cb472bec676b137d6e0358da6855c02723bf2ce
crisleo-P01 <cristianleopaiva@gmail.com>
2025-10-23 09:01:59 -0300

    Iniciando las pruebas atómicas sobre GifsApp

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,13 +1,13 @@
 interface Props {
   title: string;
-  description: string;
+  description?: string;
 }
 
 export const CustomHeader = ({ title, description }: Props) => {
   return (
     <div className="content-center">
       <h1>{title}</h1>
-      <p>{description}</p>
+      {description && <p>{description}</p>}
     </div>
   );
 };
